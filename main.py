a56b5d3bed7e4f231643e6a1242b71cb93e4b74a
JAK343 <jamesknight27@outlook.com>
2023-12-11 16:04:44 +0000

    Timer added and answer not counted if exceeded

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
 from string import ascii_lowercase
+import time
 
 Questions = {"In what year did WW1 begin?": ["1914", "1917", "1920", "1923"],
              "How many wives did Henry VIII have?": ["6", "3", "0", "7"],
@@ -30,16 +32,26 @@ def playQuiz(scoreboard, numberOfQuestions, finalQuestionCount):
         print(f"{question}")
         for label, response in labeled_answers.items():
             print(f"{label}: {response}")
+
+        timer = True
+        t1 = time.time()
         answer_index = input("Please choose a, b, c, or d...\n")
-        answer = labeled_answers[answer_index]
-        if answer == correct_answer:
-            print("Correct! Well done!")
-            scoreboard += 1
-            print(f"{scoreboard}")
-        else:
-            print("Nope, that's not right...")
+        t2 = time.time()
+        t = math.floor(t2 - t1)
+         
+        if t >= 10:
+            print("You ran out of time! Next question...")
+            timer = False                    
+        if timer == True:
+            answer = labeled_answers[answer_index]
+            if answer == correct_answer:
+                print("Correct! Well done!")
+                scoreboard += 1
+            else:
+                print("Nope, that's not right...")
+            timer = False
         finalQuestionCount += 1
-        
+
     print(f"Your final score is {scoreboard} out of {finalQuestionCount}")
     if scoreboard / finalQuestionCount * 100 > 75:
         print(f"Wow! Very impressive!")
