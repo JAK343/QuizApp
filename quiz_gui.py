bf64e5f6c7d0e7b19b9e7bc1e12b8a25cd37d6fb
JAK343 <jamesknight27@outlook.com>
2023-12-15 08:57:54 +0000

    Next button blue, centred title, question, and answer

--- quiz_gui.py
+++ quiz_gui.py
@@ -10,60 +10,62 @@ class Quiz:
         self.q_number = 0
         self.display_title()
         self.display_question()
-        self.opt_selected = IntVar()
+        self.chosen_answer = IntVar()
         self.opts= self.radio_buttons()
         self.display_options()
         self.buttons()
-        self.data_size=len(question)
+        self.finalQuestionCount=len(question)
         self.correct=0
 
     def display_title(self):
-        title = Label(root, text="General knowledge quiz", width=50, bg="blue", fg="white", font=("ariel", 20) )
-        title.place(x=0, y=2)
+        title = Label(root, text="General knowledge quiz", width=55, fg="black", font=("ariel", 20))
+        title.place(x=5, y=5)
 
     def display_question(self):
         q_number = Label(root, text=question[self.q_number], width=60, font=('ariel' , 16, 'bold'), anchor='w' )
-        q_number.place(x=70, y=100)
+        q_number.place(x=300, y=100)
 
     def radio_buttons(self):
         q_list = []
         y_pos = 150
         while len(q_list) < 4:
-            radio_btn = Radiobutton(root, text="", variable=self.opt_selected,
+            radio_btn = Radiobutton(root, text="", variable=self.chosen_answer,
             value = len(q_list) + 1, font = ("ariel", 14))
 
             q_list.append(radio_btn)
-            radio_btn.place(x = 100, y = y_pos)
+            radio_btn.place(x = 300, y = y_pos)
             y_pos += 40
         return q_list
     
     
     def display_options(self):
         val=0
-        self.opt_selected.set(0)
+        self.chosen_answer.set(0)
 
         for option in options[self.q_number]:
             self.opts[val]['text']=option
             val+=1
 
     def buttons(self):
-        next_button = Button(root, text="Next", command=self.next_btn, width=10, bg="red", fg="white", font=("ariel", 16, "bold"))
+        next_button = Button(root, text="Next", command=self.next_btn, width=10, bg="blue", fg="white", font=("ariel", 16, "bold"))
         next_button.place(x=350, y=380)
-        quit_button= Button(root, text="Quit", command=root.destroy, width=5, bg="red", fg="white", font=("ariel", 16, "bold"))
-        quit_button.place(x=700, y=50)
 
     def displayResult(self):
-        wrong_count = self.data_size - self.correct
-        correct = f"You got {self.correct} correct"
-        wrong = f"You got {wrong_count} wrong"
+        scoreboard = int(self.correct)
+        
+        if scoreboard / self.finalQuestionCount * 100 > 75:
+            message = f"You scored {scoreboard} out of {self.finalQuestionCount}. Wow! Very impressive!"
+        elif scoreboard / self.finalQuestionCount * 100 > 50:
+            message = f"You scored {scoreboard} out of {self.finalQuestionCount}. Pretty good. Still some room for improvement"
+        elif scoreboard / self.finalQuestionCount * 100 > 25:
+            message = f"You scored {scoreboard} out of {self.finalQuestionCount}. You got some right, but not the best score ever..."
+        else:
+            message = f"You scored {scoreboard} out of {self.finalQuestionCount}. Better luck next time..."
 
-        score = int(self.correct / self.data_size * 100)
-        result = f"You scored {score}%"
+        mb.showinfo("Result", f"{message}")
 
-        mb.showinfo("Result", f"{result}\n{correct}\n{wrong}")
-
-    def check_answer(self, q_no):
-        if self.opt_selected.get() == answer[q_no]:
+    def check_answer(self, q_number):
+        if self.chosen_answer.get() == answer[q_number]:
             return True
         
     def next_btn(self):
@@ -72,7 +74,7 @@ class Quiz:
 
         self.q_number += 1
 
-        if self.q_number == self.data_size:
+        if self.q_number == self.finalQuestionCount:
             self.displayResult()
             root.destroy()
         else:
@@ -81,6 +83,7 @@ class Quiz:
 
 
 # Create root window
+
 root = Tk()
 # Root window 
 root.title("General knowledge quiz")
